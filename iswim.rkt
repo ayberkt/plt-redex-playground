234707fd7437201268b956725d0747c603627a1f
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-06-16 22:20:55 -0500

    Add iswim.rkt

---
+++ iswim.rkt
@@ -0,0 +1,44 @@
+#lang racket
+(require redex)
+
+(define-language iswim
+  ((M N L K) X (λ X M) (M M) b (o2 M M) (o1 M))
+  (o o1 o2)
+  (o1 add1 sub1 iszero)
+  (o2 + - *)
+  (b number)
+  ((V U W) b X (λ X M))
+  (E hole (V E) (E M) (o V ... E M ...))
+  ((X Y Z) variable-not-otherwise-mentioned))
+
+(define-metafunction iswim
+  [(subst-var (any_1 ...) variable_1 variable_2)
+   ((subst-var any_1 variable_1 variable_2) ...)]
+  [(subst-var variable_1 variable_1 variable_2) variable_2]
+  [(subst-var any_1 variable_1 variable_2) any_1])
+
+(define-metafunction iswim
+  ;; 1. X_1 bound, so don't continue in λ body.
+  [(subst (λ X_1 any_1) X_1 any_2) (λ X_1 any_1)]
+  ;; 2. do capture avoiding substitution by generating a fresh name.
+  [(subst (λ X_1 any_1) X_2 any_2)
+   (λ X_3
+     (subst (subst-var any_1 X_1 X_3) X_2 any_2))
+   (where X_3 ,(variable-not-in (term (X_2 any_1 any_2))
+                                (term X_1)))]
+  [(subst X_1 X_1 any_1) any_1]
+  [(subst (any_2 ...) X_1 any_1)
+   ((subst any_2 X_1 any_1) ...)]
+  [(subst any_2 X_1 any_1) any_2])
+
+(define-metafunction iswim
+  [(δ (add1 b)) ,(add1 (term b))]
+  [(δ (+ b_1 b_2)) ,(+ (term b_1) (term b_2))])
+
+(define iswim-red
+  (reduction-relation
+   iswim
+   (--> (in-hole E ((λ X M) V)) (in-hole E (subst M X V)) βv)
+   (--> (in-hole E (o b ...))   (in-hole E (δ (o b ...))) δ)))
+
+(traces iswim-red (term ((λ x (+ x 12)) 1)))
